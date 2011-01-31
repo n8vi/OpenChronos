c8bc58ca9b44418810f77f30e2a2d11ecef2bab5
n8vi <n8vi@rfc791.org>
2011-01-30 23:34:59 -0500

    You like JDN?  I added that now too, at zero code size cost.

--- driver/rtc.h
+++ driver/rtc.h
@@ -37,18 +37,21 @@ extern human_date *read_rtc();
 extern void write_rtc(human_date *hd);
 extern u32 get_leapseconds_utc(machine_date ud);
 extern u32 get_leapseconds_tai(machine_date td);
-extern u32 get_offset_local(machine_date lt);
-extern u32 get_offset_utc(machine_date ut);
+extern u32 get_tz_offset_local(machine_date lt);
+extern u32 get_tz_offset_utc(machine_date ut);
 
 extern machine_date human_to_machine_date(human_date *hd);
 extern human_date *machine_to_human_date(machine_date md);
 
-#define utc_to_tai_date(ud)   ( ud + get_leapseconds_utc(ud) )
-#define tai_to_utc_date(td)   ( td + get_leapseconds_tai(td) )
-#define local_to_utc_date(ld) ( ld + get_offset_local   (ld) )
-#define utc_to_local_date(ud) ( ud + get_offset_utc     (ud) )
-#define tai_to_local_date(ld) ( utc_to_local_date ( tai_to_utc_date (ld) ) )
-#define local_to_tai_date(ud) ( utc_to_tai_date ( local_to_utc_date (ud) ) )
+#define utc_to_tai_machine_date(ud)   ( ud + get_leapseconds_utc(ud) )
+#define tai_to_utc_machine_date(td)   ( td + get_leapseconds_tai(td) )
+#define local_to_utc_machine_date(ld) ( ld + get_offset_local   (ld) )
+#define utc_to_local_machine_date(ud) ( ud + get_offset_utc     (ud) )
+#define tai_to_local_machine_date(ld) ( utc_to_local_date ( tai_to_utc_date (ld) ) )
+#define local_to_tai_machine_date(ud) ( utc_to_tai_date ( local_to_utc_date (ud) ) )
+
+#define utc_machine_date_to_jdn(ud) ( (ud / 86400) + 2440587 + ((ud % 86400)/43200) )
+#define utc_machine_date_to_jdn_fracpart(ud) ( (((ud+43200)%86400)*10000)/864  ) /* 6 digit precision */
 
 #define get_now_human_utc_date          ( machine_to_human_date(                 (human_to_machine_date(read_rtc(  )))) )
 #define get_now_human_local_date        ( machine_to_human_date(utc_to_local_date(human_to_machine_date(read_rtc(  )))) ) 
